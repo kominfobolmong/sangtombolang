d4161b2b7d67fdc0e9eed9fc4f9929bd8364beff
troydimarzio <troymokoagow86@gmail.com>
2022-06-16 15:51:53 +0800

    title dinamis

--- resources/views/lolak/layout/head.blade.php
+++ resources/views/lolak/layout/head.blade.php
@@ -2,7 +2,11 @@
 <meta charset="utf-8">
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-<title>Website Kecamatan Poigar</title>
+@forelse($namakecamatan as $item)
+<title>Website Kecamatan {{ Str::ucfirst($item->nama_kecamatan) ?? null }}</title>
+@empty
+
+@endforelse
 <meta content="" name="description">
 <meta content="" name="keywords">
 
